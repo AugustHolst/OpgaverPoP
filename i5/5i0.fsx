9250e21f65f4b41c45f1e0d0e7c837e61f95794e
Sluggan <34474922+AugustHolst@users.noreply.github.com>
2018-10-07 22:31:52 +0200

    5i0.fsx
    mangler sidste delopgave

---
+++ i5/5i0.fsx
@@ -0,0 +1,30 @@
+let exampleList = [[1; 2; 3]; [4; 5; 6]]
+let incorrectList1 = [[1; 2; 3];[4; 5; 6];[]]
+let incorrectList2 = [[1; 2; 3];[4; 5; 6];[0;0;4;3]]
+
+let isTable (table : 'a list list) : bool = 
+    let firstRowLen = table.Head.Length
+    let boolConjunct acc (row : 'a list) = not row.IsEmpty && row.Length = firstRowLen && acc    
+    List.fold boolConjunct true table
+(*
+printfn "%b" (isTable exampleList)
+printfn "%b" (isTable incorrectList1)
+printfn "%b" (isTable incorrectList2)
+*)
+let firstColumn (table : 'a list list) : 'a list = 
+    List.map (fun (x : 'a list) -> x.Head) table
+
+printfn "%A" (firstColumn exampleList)
+
+let dropFirstColumn (table : 'a list list) : 'a list list = 
+    List.map (fun (x : 'a list) -> x.Tail) table
+
+printfn "%A" (dropFirstColumn exampleList)
+
+(*
+let transpose (table : 'a list list) : 'a list list =
+    let mutable acc = table
+    List.map (fun x -> acc <- (firstColumn(dropFirstColumn acc))) table
+
+printf "%A" (transpose exampleList)
+*)
